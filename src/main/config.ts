9185fa5196cc7a0fcadb72cbae9d70d74870d283
mseynaeve <matthieu.seynaeve@laposte.fr>
2022-08-30 10:42:06 +0200

    Add nodeConfig

--- src/main/config.ts
+++ src/main/config.ts
@@ -1,13 +1,6 @@
 import * as nodeConfig from 'config'
 import { Config } from "./types/config";
 
-const cluster = nodeConfig.has('cluster') ? nodeConfig.get<Config['cluster']>('cluster') : { workers: 0 }
+export const cluster = nodeConfig.has('cluster') ? nodeConfig.get<Config['cluster']>('cluster') : { workers: 0 }
 
-const log = nodeConfig.has('log') ? nodeConfig.get<Config['log']>('log') : { name: 'Express-template' }
-
-const config: Config = {
-  cluster,
-  log
-}
-
-export = config
+export const log = nodeConfig.has('log') ? nodeConfig.get<Config['log']>('log') : { name: 'Express-template' }
